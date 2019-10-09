c70947cf01a632db4ab10f1ed6c2f04fae9329a3
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-10-08 19:57:37 -0700

    fix logic to calculate remaining guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,17 @@
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var computerChoice = Math.floor(Math.random() * alphabet.length);
 var userChoice;
 var userChoiceLocation = document.getElementById("userGuess")
 var remainingGuessLocation = document.getElementById("remainingGuess")
 var remainingGuess = 10;
 
-document.onkeyup = function(event) {
-    
+document.onkeyup = function (event) {
+
     var userChoice = event.key
     userChoiceLocation.innerHTML = userChoice
-    remainingGuessLocation.innerHTML = remainingGuess - 1;
+    remainingGuess = remainingGuess - 1
+    remainingGuessLocation.innerHTML = remainingGuess
 
 }
