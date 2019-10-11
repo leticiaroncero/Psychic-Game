75c0cb96975d3c6cbdd45868fca1751466dc41e0
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-10-10 21:48:14 -0700

    add code to display all guesses in a single line

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,21 +2,22 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
     "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var randomPosition = Math.floor(Math.random() * alphabet.length);
-var computerChoice = alphabet[randomPosition]
-var userChoice;
-var userChoiceLocation = document.getElementById("userGuess")
-var remainingGuessLocation = document.getElementById("remainingGuess")
+var computerChoice = alphabet[randomPosition];
+var userChoiceLocation = document.getElementById("userGuess");
+var remainingGuessLocation = document.getElementById("remainingGuess");
 var remainingGuess = 10;
-var winsLocation = document.getElementById("wins")
+var winsLocation = document.getElementById("wins");
 var wins = 0;
 var losses = 0;
 var lossesLocation = document.getElementById("losses");
 
-
 document.onkeyup = function (event) {
 
     var userChoice = event.key.toLowerCase()
-    userChoiceLocation.innerHTML = userChoice
+    var addChoice = document.createElement("span")
+    addChoice.innerHTML = userChoice
+    userChoiceLocation.appendChild(addChoice)
+
     remainingGuess = remainingGuess - 1
     remainingGuessLocation.innerHTML = remainingGuess
 
